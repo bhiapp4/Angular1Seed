dd5d10f47c41228a66f5c1d1d9580403ab241541
Appi Bhimavarapu <appi.bh@gmail.com>
2016-08-09 22:53:47 -0500

    Added UI routing to the application

--- app/app.js
+++ app/app.js
@@ -1,9 +1,32 @@
-var app = angular.module('SeedApp',[]);
+var app = angular.module('SeedApp',['ui.router']);
 
-app.service('seedService',function($http){
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/page1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('page1', {
+      url: '/page1',
+      templateUrl: 'pages/page1.html',
+      controller: 'Page1Controller'
+    })   
+    .state('page2', {
+      url: '/page2',
+      templateUrl: 'pages/page2.html',
+      controller: 'Page2Controller'
+    });
+});
+
+app.service('PageService',function($http){
     
 });
 
-app.controller('seedController',function(seedService, $scope){
-    $scope.value = 'Hello Angular 1';
+app.controller('Page1Controller',function(PageService, $scope){
+    $scope.value = 'Hello Page 1';
+});
+
+app.controller('Page2Controller',function(PageService, $scope){
+    $scope.value = 'Hello Page 2';
 });
